80128c54d36af1a5207d7e0847e7a1e2771db31b
emorilebo <emorylebo@gmail.com>
2021-10-13 22:19:46 +0100

    Stories with scrollbar implementation

--- components/Feed.js
+++ components/Feed.js
@@ -1,14 +1,18 @@
+import Stories from "./Stories";
+
 function Feed() {
   return (
-    <main>
-      <h1>Feed</h1>
-      {/* Section */}
-      {/* Stories */}
-      {/* Posts */}
+    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto">
+      <section className="col-span-2">
+        {/* Stories */}
+        <Stories />
+        {/* Posts */}
+      </section>
 
-      {/* Sectiont */}
-      {/* Mini profile */}
-      {/* Suggestions */}
+      <section>
+        {/* Mini profile */}
+        {/* Suggestions */}
+      </section>
     </main>
   );
 }
