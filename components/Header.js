2ee81dc0438b6458a9b979aee548671262e8670c
emorilebo <emorylebo@gmail.com>
2021-10-13 10:39:15 +0100

    Search implement

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,26 @@
 import Image from "next/image";
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserCircleIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
     <div>
       <div className="flex justify-between mx-w-6xl">
         {/* Left */}
-        <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
           <Image
             src="https://links.papareact.com/ocw"
             layout="fill"
             objectFit="contain"
           />
         </div>
-        <div className="relative w-10 h-10 lg:hidden cursor-pointer">
+        <div className="relative w-10 lg:hidden cursor-pointer">
           <Image
             src="https://links.papareact.com/jjm"
             layout="fill"
@@ -21,6 +29,16 @@ function Header() {
         </div>
 
         {/* Middle */}
+        <div className="relative mt-1 p-3 rounded-md">
+          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+            <SearchIcon className="h-5 w-5 text-gray-500" />
+          </div>
+          <input
+            className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
 
         {/* Right */}
       </div>
