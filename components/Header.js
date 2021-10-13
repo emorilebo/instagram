4d37216ba4750277cf5fdc61ff8e2162f6807a07
emorilebo <emorylebo@gmail.com>
2021-10-13 09:43:12 +0100

    Logo image implement

---
+++ components/Header.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+function Header() {
+  return (
+    <div>
+      <div className="flex justify-between mx-w-6xl">
+        {/* Left */}
+        <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="relative w-10 h-10 lg:hidden cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        {/* Middle */}
+
+        {/* Right */}
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -7,8 +8,8 @@ export default function Home() {
         <title>Instagram By Godfrey Lebo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Building Instagram</h1>
       {/* Header */}
+      <Header />
       {/* Feed */}
       {/* Modal */}
     </div>
