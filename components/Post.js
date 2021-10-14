a5acc69c1188b4091e2928ccb787d1846c37e7fb
emorilebo <emorylebo@gmail.com>
2021-10-14 15:11:44 +0100

    Feed implementation

--- components/Post.js
+++ components/Post.js
@@ -1,10 +1,25 @@
+import {
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  EmojiHappyIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+
 function Post({ id, username, userImg, img }) {
   return (
     <div>
       {/* Header */}
-      <div>
-        <img src="" alt="" />
+      <div className="flex">
+        <img
+          src={userImg}
+          className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+          alt=""
+        />
         <p>{username}</p>
+        <DotsHorizontalIcon className="h-5" />
       </div>
       {/* Img */}
       {/* Buttons */}
