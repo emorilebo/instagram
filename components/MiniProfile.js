62bfa7db1e3b7b8812f5b7db1890193ca5845368
emorilebo <emorylebo@gmail.com>
2021-10-15 21:14:31 +0100

    miniprofile session and suggestion hidden bug fix

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -7,7 +7,7 @@ function MiniProfile() {
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
         className="w-16 h-16 rounded-full border p-[2px]"
-        src={session?.user.image}
+        src={session?.user?.image}
         alt=""
       />
       <div className="flex-1 mx-4">
