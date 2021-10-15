e13a844806f47fb0003b1d1881f5d92cec700cfe
emorilebo <emorylebo@gmail.com>
2021-10-15 07:47:24 +0100

    Suggestion details

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -17,6 +17,24 @@ function Suggestions() {
         <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
         <button className="text-gray-600 font-semibold">See All</button>
       </div>
+      {suggestions.map((profile) => (
+        <div
+          key={profile.id}
+          className="flex items-center justify-between mt-3"
+        >
+          <img
+            className="w-10 h-10 rounded-full border p-[2px]"
+            src={profile.avatar}
+            alt=""
+          />
+          <div className="flex-1 ml-4 ">
+            <h2 className="font-semibold text-sm">{profile.username}</h2>
+            <h3 className="text-xs text-gray-400">
+              Works at {profile.company.name}
+            </h3>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
