7cd49db4fe3d92bf27c4c878c700fd06d8508b1b
emorilebo <emorylebo@gmail.com>
2021-10-18 11:42:58 +0100

    Modal implementation

--- components/Modal.js
+++ components/Modal.js
@@ -1,13 +1,37 @@
 import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
 function Modal() {
   const [open, setOpen] = useRecoilState(modalState);
 
   return (
-    <Transition>
-      <h1></h1>
-    </Transition>
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed z-10 inset-0 overscroll-y-auto"
+        onClose={setOpen}
+      >
+        <div className="flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+          {/* This element is to trick the browser into centering the modal contents */}
+          <span className="hidden sm:inline-block sm:align-middle sm:h-screen">
+            &#8203
+          </span>
+        </div>
+      </Dialog>
+    </Transition.Root>
   );
 }
 
