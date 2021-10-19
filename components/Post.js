a0795302ab539ce90ec3324998660e52e2ed5dfb
emorilebo <emorylebo@gmail.com>
2021-10-19 09:10:00 +0100

    number of likes

--- components/Post.js
+++ components/Post.js
@@ -118,6 +118,9 @@ function Post({ id, username, userImg, img, caption }) {
 
       {/* Caption */}
       <p className="p-5 truncate">
+        {likes.length > 0 && (
+          <p className="font-bold mb-1">{likes.length} likes</p>
+        )}
         <span className="font0bold mr-1">{username} </span>
         {caption}
       </p>
